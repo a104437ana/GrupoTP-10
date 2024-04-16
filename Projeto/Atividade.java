04726a9569aa2cc5ce967acfc235e78867503ed7
Inês Marques <a104263@alunos.uminho.pt>
2024-04-16 17:22:50 +0100

    Adicionada classe Atividade

---
+++ Projeto/Atividade.java
@@ -0,0 +1,82 @@
+
+/**
+ * Write a description of class Atividade here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public abstract class Atividade
+{
+    // variáveis de instância
+    private int tempo;
+    private int freqCardiaca;
+
+    /**
+     * Construtores de Atividade
+     *
+     * Atividade é uma classe abstrata, logo, os construtores só vão ser invocados pelas suas sub-classes nos seus próprios construtores, e não para criar instâncias de Atividade
+     */
+    
+    /**
+     * Construtor vazio
+     */
+    public Atividade()
+    {
+        this.tempo = 0;
+        this.freqCardiaca = 0;
+    }
+    
+    /**
+     * Construtor parametrizado
+     */
+    public Atividade(int tempo, int freqCardiaca)
+    {
+        this.tempo = tempo;
+        this.freqCardiaca = freqCardiaca;
+    }
+    
+    /**
+     * Construtor de cópia
+     */
+    public Atividade(Atividade umaAtividade)
+    {
+        this.tempo = umaAtividade.getTempo();
+        this.freqCardiaca = umaAtividade.getFreqCardiaca();
+    }
+    
+    public int getTempo(){
+        
+    }
+
+    public int getFreqCardiaca(){
+        
+    }
+
+    public void setTempo(int tempo){
+        
+    }
+    
+    public void setFreqCardiaca(int freqCardiaca){
+        
+    }
+    
+    /**
+     * Método que calcula o consumo de calorias de uma atividade, da maneira definida para essa atividade - deve ser implementado pelas sub-classes
+     *
+     * @param  utilizador  utilizador que realiza a atividade
+     * @return    consumo de calorias da atividade
+     */
+    public abstract int consumoCalorias(Utilizador utilizador);
+
+    public String toString(){
+        
+    }
+
+    public boolean equals(Object o){
+        
+    }
+
+    public Atividade clone(){
+        
+    }
+}
