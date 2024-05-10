bc1c7323c5551e988abf183b07444f4c137717c5
Inês Marques <a104263@alunos.uminho.pt>
2024-05-10 13:21:58 +0100

    Continuação dos menus, adicionada documentação

--- Projeto/AppDesportiva.java
+++ Projeto/AppDesportiva.java
@@ -1,38 +1,51 @@
 package Projeto;
 import java.io.*;
+import java.time.*;
 
 /**
- * Classe AppDesportiva, que corresponde ao programa principal e ao controller
+ * Classe AppDesportiva, que corresponde ao controller
  *
  * @author Grupo10
- * @version 09/05/24
- * Notas versão : Incompleta
+ * @version 10/05/24
+ * Notas versão :
  */
 public class AppDesportiva
 {
     private GestorDesportivo model;
     private Menu menuSetup, menuApp, menuEstatisticas;
-    
+    private static String[] opcoesSetup = {"Menu Principal","Carregar estado", "Adicionar utilizador", "Adicionar atividade", "Adicionar plano de treino", "Começar fase de simulação", "Guardar estado", "Guardar e sair","Sair"};
+    private static String[] opcoesApp = {"Menu","Adicionar utilizador", "Registar atividade", "Adicionar plano de treino", "Alterar data", "Mostrar informações", "Consultar estatísticas", "Guardar estado", "Voltar ao setup", "Guardar e sair", "Sair"};
+    private static String[] opcoesQueries = {"Estatísticas","Recorde calorias gastas", "Recorde atividades realizadas", "Atividade mais realizada", "Kms percorridos", "Metros de altimetria", "Record plano de treino", "Atividades utilizador", "Voltar"};
+   
     /**
      * Construtor de AppDesportiva
      */
     public AppDesportiva()
     {
         this.model = new GestorDesportivo();
-        String[] opcoesSetup = {"Carregar estado", "Adicionar utilizador", "Adicionar atividade", "Adicionar plano de treino", "Começar fase de simulação", "Guardar estado", "Guardar e sair"};
-        this.menuSetup = new Menu(opcoesSetup);
-        String[] opcoesApp = {"Adicionar utilizador", "Registar atividade", "Adicionar plano de treino", "Alterar data", "Mostrar informações", "Consultar estatísticas", "Guardar estado", "Voltar ao setup", "Guardar e sair"};
-        this.menuApp = new Menu(opcoesApp);
-        String[] opcoesQueries = {"Recorde calorias gastas", "Recorde atividades realizadas", "Atividade mais realizada", "Kms percorridos", "Metros de altimetria", "Record plano de treino", "Atividades utilizador"};
-        this.menuEstatisticas = new Menu(opcoesQueries);
+        this.menuSetup = new Menu(this.opcoesSetup);
+        this.menuApp = new Menu(this.opcoesApp);
+        this.menuEstatisticas = new Menu(this.opcoesQueries);
     }
     
     /**
      * Construtor de AppDesportiva quando é passado o nome do ficheiro a carregar
+     * @param nome do ficheiro
      */
     public AppDesportiva(String ficheiro)
     {
         this.model = new GestorDesportivo();
+        this.carregaDados(ficheiro);
+        this.menuSetup = new Menu(this.opcoesSetup);
+        this.menuApp = new Menu(this.opcoesApp);
+        this.menuEstatisticas = new Menu(this.opcoesQueries);
+    }
+    
+    /**
+     * Método auxiliar carregaDados
+     * @param nome do ficheiro
+     */
+    private void carregaDados(String ficheiro){
         try{
         this.model.carregaEstado(ficheiro);
         }
@@ -42,46 +55,38 @@ public class AppDesportiva
         catch (IOException ioExc){
             System.out.println("Não foi possível ler o ficheiro");
         }
-        String[] opcoesSetup = {"Carregar estado", "Adicionar utilizador", "Adicionar atividade", "Adicionar plano de treino", "Começar fase de simulação", "Guardar estado" , "Guardar e sair"};
-        this.menuSetup = new Menu(opcoesSetup);
-        String[] opcoesApp = {"Adicionar utilizador", "Registar atividade", "Adicionar plano de treino", "Alterar data", "Mostrar informações", "Consultar estatísticas", "Guardar estado", "Voltar ao setup", "Guardar e sair"};
-        this.menuApp = new Menu(opcoesApp);
-        String[] opcoesQueries = {"Recorde calorias gastas", "Recorde atividades realizadas", "Atividade mais realizada", "Kms percorridos", "Metros de altimetria", "Record plano de treino", "Atividades utilizador"};
-        this.menuEstatisticas = new Menu(opcoesQueries);
     }
     
     /**
-     * Método main
+     * Método auxiliar guardaDados
+     * @param nome do ficheiro
      */
-    public static void main(String[] args){
-        if(args.length==0) new AppDesportiva().runSetup();
-        else new AppDesportiva(args[0]).runSetup();
+    private void guardaDados(String ficheiro){
+        try {
+            this.model.guardaEstado(ficheiro);
+        }
+        catch (IOException e){
+            System.out.println("Não foi possível guardar o estado");
+        }
     }
     
     /**
-     * Método runSetup, que executa o menu inicial, lê e trata as opções escolhidas, enviando a operação pretendida para o model, ou encerrando o programa
+     * Método runSetup, que executa o menu inicial, lê e trata as opções escolhidas, enviando a operação pretendida para o model, indo para outro menu, ou encerrando o programa
      */
     public void runSetup(){
         menuSetup.runMenu();
         int op = menuSetup.getOpcao();
         while (op!=0){
-            switch (op){
-                case 1 :   
+            switch (op){ 
+
+                case 1 :   //opção "Carregar estado"
                     String ficheiro = menuSetup.pedeInput("Insira o nome do ficheiro a carregar");
-                    try{
-                        this.model.carregaEstado(ficheiro);
-                    }
-                    catch (ClassNotFoundException clExc){
-                        System.out.println("Não foi possível carregar os dados do ficheiro");
-                    }
-                    catch (IOException ioExc){
-                        System.out.println("Não foi possível ler o ficheiro");
-                    }
+                    this.carregaDados(ficheiro);
                     break;
-                case 2 : ;
-                case 3 : ;
-                case 4 : ;
-                case 5 : 
+                case 2 : ; //opção "Adicionar utilizador"
+                case 3 : ; //opção "Adicionar atividade"
+                case 4 : ; //opção "Adicionar plano de treino"
+                case 5 :    //opção "Começar fase de simulação"
                     {
                         int ext = this.runApp();
                         if(ext == 2){
@@ -89,33 +94,18 @@ public class AppDesportiva
                         }
                         else if (ext == 1){
                             String nomeFicheiro = menuSetup.pedeInput("Insira o nome do ficheiro para guardar");
-                            try {
-                                this.model.guardaEstado(nomeFicheiro);
-                            }
-                            catch (IOException e){
-                                System.out.println("Não foi possível guardar o estado");
-                            }
+                            this.guardaDados(nomeFicheiro);
                             op=0;
                         }
                     }
                     break;
-                case 6 :
+                case 6 :    //opção "Guardar estado"
                     String file = menuSetup.pedeInput("Insira o nome do ficheiro para guardar");
-                    try {
-                        this.model.guardaEstado(file);
-                    }
-                    catch (IOException e){
-                        System.out.println("Não foi possível guardar o estado");
-                    }
+                    this.guardaDados(file);
                     break;
-                case 7 :
+                case 7 :    //opção "Guardar e sair"
                     String saveFile = menuSetup.pedeInput("Insira o nome do ficheiro para guardar");
-                    try {
-                        this.model.guardaEstado(saveFile);
-                    }
-                    catch (IOException e){
-                        System.out.println("Não foi possível guardar o estado");
-                    }
+                    this.guardaDados(saveFile);
                     op=0;
                     break;
             }
@@ -128,24 +118,28 @@ public class AppDesportiva
     }
     
     /**
-     * Método 
+     * Método runApp que executa o menu depois de iniciada a simulação
+     * @return código de saída para o menu setup: 0 - não sair, 1 - guardar e sair, 2 - sair sem guardar
      */
     public int runApp(){
         menuApp.runMenu();
         int op = menuApp.getOpcao();
         while (op!=0){
             switch (op){
-                case 1 : ;
-                case 2 : ;
-                case 3 : ;
-                case 4 : ;
-                case 5 : ;
-                case 6 : 
+                case 1 : ; //opção "Adicionar utilizador"
+                case 2 : ; //opção "Registar atividade"
+                case 3 : ; //opção "Adicionar plano de treino"
+                case 4 : ; //opção "Alterar data"
+                case 5 : ; //opção "Mostrar informações"
+                case 6 :    //opção "Consultar estatísticas"
                     this.runQueries();
                     break;
-                case 7 : ;
-                case 8 : return 0;
-                case 9 : return 1;
+                case 7 :    //opção "Guardar estado"
+                    String nomeFicheiro = menuApp.pedeInput("Insira o nome do ficheiro para guardar");
+                    this.guardaDados(nomeFicheiro);
+                    break;
+                case 8 : return 0;  //opção "Voltar ao setup"
+                case 9 : return 1; //opção "Guardar e sair"
             }
             menuApp.runMenu();
             op = menuApp.getOpcao();
@@ -153,13 +147,26 @@ public class AppDesportiva
         return 2;
     }
     
+    /**
+     * Método runApp que executa o menu de estatísticas
+     */
     public void runQueries(){
         this.menuEstatisticas.runMenu();
         int op = this.menuEstatisticas.getOpcao();
         while (op!=0){
             switch (op){
-                case 1 : ;
-                case 2 : ;
+                case 1 :    //opção "Recorde calorias gastas"
+                    LocalDate dataInicial = LocalDate.parse(this.menuEstatisticas.pedeInput("Insira data inicial"));
+                    LocalDate dataFinal = LocalDate.parse(this.menuEstatisticas.pedeInput("Insira data final"));
+                    Utilizador res = this.model.maisCaloriasGastas(dataInicial,dataFinal);
+                    System.out.println(res.toString());
+                    break;
+                case 2 :    //opção "Recorde atividades realizadas"
+                    LocalDate dataInicio = LocalDate.parse(this.menuEstatisticas.pedeInput("Insira data inicial"));
+                    LocalDate dataFim = LocalDate.parse(this.menuEstatisticas.pedeInput("Insira data final"));
+                    Utilizador user = this.model.maisAtividades(dataInicio,dataFim);
+                    System.out.println(user.toString());
+                    break;
                 case 3 : ;
                 case 4 : ;
                 case 5 : ;
--- Projeto/Menu.java
+++ Projeto/Menu.java
@@ -5,16 +5,16 @@ import java.util.*;
  * Classe Menu, que implementa uma interface em modo texto
  *
  * @author Grupo10
- * @version 09/05/24
- * Notas versão : Falta ter opções indisponíveis
+ * @version 10/05/24
+ * Notas versão :
  */
 public class Menu
 {
-    private List<String> opcoes;
+    private List<String> opcoes; //opção 0 nesta lista é o nome do menu e a última é o nome da opção de saída
     private int op;
     
     /**
-     * Construtor de Menu
+     * Construtor de Menu, recebe a lista com as opções e o nome do menu
      */
     public Menu(String[] opcoes){
         this.opcoes = Arrays.asList(opcoes);
@@ -23,6 +23,47 @@ public class Menu
     
     public int getOpcao(){
         return this.op;
+    }
+    
+    /**
+     * Método mostraMenu, que imprime as opções disponíveis
+     */
+    private void mostraMenu(){
+        //limpa a tela
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        //imprime o nome do menu
+        System.out.print("-------------");
+        System.out.print(this.opcoes.get(0));
+        System.out.print("-------------\n");
+        int i;
+        for (i=1; i<this.opcoes.size()-1; i++){
+            System.out.print("Opção ");
+            System.out.print(i);
+            System.out.print(": ");
+            System.out.println(this.opcoes.get(i));
+        }
+        System.out.print("Opção 0: ");
+        System.out.println(this.opcoes.get(i));
+    }
+    
+    /**
+     * Método lerOpcao, que lê a opção selecionada
+     */
+    private int lerOpcao(){
+        int op;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Opção: ");
+        try{
+            op = scan.nextInt();
+        }
+        catch (InputMismatchException e){
+            op = -1;
+        }
+        if(op<0 || op>this.opcoes.size()-1){
+            System.out.println("Opção inválida");
+            op=-1;
+        }
+        return op;
     }
     
     /**
@@ -37,43 +78,9 @@ public class Menu
     }
     
     /**
-     * Método mostraMenu, que imprime as opções disponíveis
-     */
-    public void mostraMenu(){
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-        System.out.println("-------------Menu-------------");
-        int i;
-        for (i=0; i<this.opcoes.size(); i++){
-            System.out.print("Opção ");
-            System.out.print(i+1);
-            System.out.print(": ");
-            System.out.println(this.opcoes.get(i));
-        }
-        System.out.println("Opção 0: Sair");
-    }
-    
-    /**
-     * Método lerOpcao, que lê a opção selecionada
-     */
-    public int lerOpcao(){
-        int op;
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Opção: ");
-        try{
-            op = scan.nextInt();
-        }
-        catch (InputMismatchException e){
-            op = -1;
-        }
-        if(op<0 || op>this.opcoes.size()){
-            System.out.println("Opção inválida");
-            op=-1;
-        }
-        return op;
-    }
-    
-    /**
     * Método pedeInput
+    * @param mensagem: mensagem para mostrar ao utilizador
+    * @returns próxima linha inserida
     */
     public String pedeInput(String mensagem){
         System.out.println(mensagem);
